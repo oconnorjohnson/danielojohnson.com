697624b247420e020d7c898f6cf0a8930b05d566
Daniel Johnson <dojohnson@ucdavis.edu>
2024-04-23 13:29:18 -0700

    started setup of drizzle, turso set up

--- server/db/tursoClients.ts
+++ server/db/tursoClients.ts
@@ -1,12 +1,17 @@
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/libsql";
 import { createClient as createNodeClient } from "@libsql/client";
 import { createClient as createEdgeClient } from "@libsql/client/web";
 
-export const tursoNodeClient = createNodeClient({
+const tursoNodeClient = createNodeClient({
   url: process.env.TURSO_DATABASE_URL!,
   authToken: process.env.TURSO_AUTH_TOKEN,
 });
 
-export const tursoEdgeClient = createEdgeClient({
+const tursoEdgeClient = createEdgeClient({
   url: process.env.TURSO_DATABASE_URL!,
   authToken: process.env.TURSO_AUTH_TOKEN,
 });
+
+export const nodeDb = drizzle(tursoNodeClient);
+export const edgeDb = drizzle(tursoEdgeClient);
