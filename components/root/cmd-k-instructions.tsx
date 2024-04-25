fac1c436d14951098e81b1d6ad6f34d33879dd23
Daniel Johnson <dojohnson@ucdavis.edu>
2024-04-24 19:24:45 -0700

    added command component from shadcn

--- components/root/cmd-k-instructions.tsx
+++ components/root/cmd-k-instructions.tsx
@@ -4,11 +4,15 @@ export default function CmdKInstructions() {
       <div className="flex flex-col items-center justify-center  text-lg font-source-code-pro p-4">
         <div className="bg-gray-900 w-[350px] text-white p-4 rounded-lg shadow-md space-y-2">
           <div className="flex items-center justify-between">
-            <span className="font-semibold pr-4">Open a page</span>
+            <span className="font-semibold pr-4">Toggle Terminal</span>
+            <span className="bg-gray-700 px-2 py-1 rounded">⌘ + j</span>
+          </div>
+          <div className="flex items-center justify-between">
+            <span className="font-semibold pr-4">Open page</span>
             <span className="bg-gray-700 px-2 py-1 rounded">⌘ + p</span>
           </div>
           <div className="flex items-center justify-between">
-            <span className="font-semibold pr-4">Close a page</span>
+            <span className="font-semibold pr-4">Close page</span>
             <span className="bg-gray-700 px-2 py-1 rounded">⌘ + w</span>
           </div>
           <div className="flex items-center justify-between">
