5950eea8fd1286bfac0bdf6aedbae559ace79001
Daniel Johnson <dojohnson@ucdavis.edu>
2024-04-26 13:52:23 -0700

    edited readme, pushed code editor down route tree and reserved root for experience choosing

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,25 @@
 import Editor from "@/components/root/editor";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 import { MdOutlineClose } from "react-icons/md";
 import ProjectCarousel from "@/components/root/project-carousel";
 import BlogCarousel from "@/components/root/blog-carousel";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import ExperienceChoice from "@/components/root/experience-choice";
 
 export default function Home() {
   return (
-    <div className="p-10 w-full h-full">
-      <div className="bg-gray-800 flex flex-col">
-        <Editor />
+    <>
+      {/* phone experience */}
+      <div className="p-10 w-full h-full md:hidden ">
+        <div className="bg-gray-800 flex flex-col">
+          {/* Here we're going to have the mobile site, with a header telling the user to come back on a laptop or desktop to get a choice of experiences */}
+        </div>
       </div>
-    </div>
+      {/* code-editor experience */}
+      <div className="hidden md:block">
+        <ExperienceChoice />
+      </div>
+    </>
   );
 }
--- components/root/cmd-k-instructions.tsx
+++ components/root/cmd-k-instructions.tsx
@@ -5,19 +5,26 @@ export default function CmdKInstructions() {
         <div className="bg-gray-900 w-[400px] text-white p-4 rounded-lg shadow-md space-y-2">
           {/* <div className="flex items-center justify-between">
             <span className="font-semibold pr-4">Toggle Terminal</span>
-            <span className="bg-gray-700 px-2 py-1 rounded">⌘ + j</span>
+            <span className="flex flex-row items-center bg-gray-700 px-2 py-1 rounded">⌘ + j</span>
           </div> */}
-          <div className="flex items-center justify-between">
+          <div className="flex flex-row items-center justify-between">
             <span className="font-semibold pr-4">Open page</span>
-            <span className="bg-gray-700 px-2 py-1 rounded">⌘ + p</span>
+            <span className="bg-gray-700 px-2 py-1 rounded">
+              ⌘ <span className="text-gray-400">+</span> o
+            </span>
           </div>
-          <div className="flex items-center justify-between">
+          <div className="flex flex-row items-center justify-between">
             <span className="font-semibold pr-4">Close page</span>
-            <span className="bg-gray-700 px-2 py-1 rounded">⌘ + z</span>
+            <span className="bg-gray-700 px-2 py-1 rounded">
+              ⌘ <span className="text-gray-400">+</span> z
+            </span>
           </div>
-          <div className="flex items-center justify-between">
+          <div className="flex flex-row items-center justify-between">
             <span className="font-semibold pr-4">Close all</span>
-            <span className="bg-gray-700 px-2 py-1 rounded">⌘ + shift + z</span>
+            <span className="bg-gray-700 px-2 py-1 rounded">
+              ⌘ <span className="text-gray-400">+</span> shift{" "}
+              <span className="text-gray-400">+</span> z
+            </span>
           </div>
         </div>
       </div>
