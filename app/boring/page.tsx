df4f8d2bc657cbcdd278f0b8b1e6cb39d8f0a8d1
Daniel Johnson <dojohnson@ucdavis.edu>
2024-05-07 14:26:11 -0700

    boring page looking really good lol

--- app/boring/page.tsx
+++ app/boring/page.tsx
@@ -4,8 +4,12 @@ import { MdArrowLeft } from "react-icons/md";
 export default function BoringPage() {
   return (
     <>
-      <div id="home" className="flex flex-col w-screen h-screen">
-        <div className="flex flex-col items-center justify-center w-full">
+      <div
+        id="home"
+        className="flex flex-col w-screen bg-gray-950"
+        style={{ scrollBehavior: "smooth" }}
+      >
+        <div className="flex flex-col items-center justify-center w-full ">
           <div className="flex flex-col xl:flex-row justify-between items-center align-center w-1/2 pt-20 pb-24">
             <div className="flex flex-col items-center justify-center">
               <h1 className="font-thin text-xl lg:text-4xl pt-8 ">
@@ -27,48 +31,49 @@ export default function BoringPage() {
                 Navigation
               </h1>
               <div className="py-2" />
-              <a className="hover:underline" href="#projects">
+              <a className="hover:underline text-blue-400" href="#projects">
                 Projects
               </a>
-              <a className="hover:underline" href="#skills">
+              <a className="hover:underline text-blue-400" href="#skills">
                 Skills & Interests
               </a>
-              <a className="hover:underline" href="#blogs">
+              <a className="hover:underline text-blue-400" href="#blogs">
                 Blogs
               </a>
-              <a className="hover:underline" href="#contact">
+              <a className="hover:underline text-blue-400" href="#contact">
                 Contact
               </a>
             </div>
           </div>
-          <div className="py-6 w-full border-b border-gray-400" />
-          <div className="py-12 w-full " />
-          <h1
-            id="projects"
-            className="flex flex-col justify-center font-bold text-2xl lg:text-4xl"
-          >
-            Projects
-          </h1>
-          <div className="py-2" />
-          <div className="flex flex-col justify-center w-3/4 lg:w-1/2 pt-12">
+
+          <div className="flex flex-col justify-center w-3/4 lg:w-1/2">
+            <div className="w-full border-b border-gray-400" />
+            <div className="py-12 w-full " />
+            <h1
+              id="projects"
+              className="flex flex-col justify-center font-bold text-2xl lg:text-4xl"
+            >
+              Projects
+            </h1>
+            <div className="py-2" />
             <div className="flex flex-row justify-between w-full border-l-4 border-gray-400">
               <div className="w-1/4 lg:w-2/3  border-t border-gray-400" />
               <div className="flex flex-col border border-gray-400 rounded-bl-lg rounded-br-lg rounded-tr-lg px-6 py-4 mb-8">
-                <h2 className="font-semibold text-xl">
+                <h2 className="font-semibold text-xl pb-2 text-blue-500">
                   babylon.legal {"(WIP)"}
                 </h2>
-                <h3 className="font-light text-lg">
+                <h3 className="font-light text-lg pb-2">
                   Integrated case management for every agency of the criminal
                   justice system
                 </h3>
-                <p className="text-gray-400">
+                <p className="text-gray-300 pb-2">
                   A suite of interconnected management tools designed to
                   streamline interdepartmental communications, data transfer,
                   and data integrity across the criminal justice system.
                 </p>
                 <Link
                   href="https://babylon.legal"
-                  className="text-blue-400 underline"
+                  className="text-blue-400 hover:underline"
                 >
                   About & Waitlist
                 </Link>
@@ -77,11 +82,13 @@ export default function BoringPage() {
             <div className="flex flex-row justify-between w-full border-l-4 border-gray-400">
               <div className="w-1/4 lg:w-2/3 border-t border-gray-400" />
               <div className="flex flex-col border border-gray-400 rounded-bl-lg rounded-br-lg rounded-tr-lg px-6 py-4 mb-8">
-                <h2 className="font-semibold text-xl">
+                <h2 className="font-semibold text-xl pb-2 text-blue-500">
                   Defeat-The-Primeagen {"(WIP)"}
                 </h2>
-                <h3 className="font-light text-lg">EdgeDB Hackathon Entry</h3>
-                <p className="text-gray-400">
+                <h3 className="font-light text-lg pb-2">
+                  EdgeDB Hackathon Entry
+                </h3>
+                <p className="text-gray-300 pb-2">
                   A 2D HTML game in which you&apos;re tasked with defending your
                   database from legendary backend engineer, The Primeagen, who
                   is DDOS&apos;ing your service, threatening your company with
@@ -89,7 +96,7 @@ export default function BoringPage() {
                 </p>
                 <Link
                   href="https://github.com/oconnorjohnson/defeat-the-primeagen"
-                  className="text-blue-400 underline"
+                  className="text-blue-400 hover:underline"
                 >
                   GitHub
                 </Link>
@@ -98,17 +105,19 @@ export default function BoringPage() {
             <div className="flex flex-row justify-between w-full border-l-4 border-gray-400">
               <div className="w-1/4 lg:w-2/3  border-t border-gray-400" />
               <div className="flex flex-col border border-gray-400 rounded-bl-lg rounded-br-lg rounded-tr-lg px-6 py-4 mb-8">
-                <h2 className="font-semibold text-xl">danielojohnson.com</h2>
-                <h3 className="font-light text-lg">
+                <h2 className="font-semibold text-xl pb-2 text-blue-500">
+                  danielojohnson.com
+                </h2>
+                <h3 className="font-light text-lg pb-2">
                   Multi-Experience Personal Portfolio
                 </h3>
-                <p className="text-gray-400">
+                <p className="text-gray-300 pb-2">
                   My latest portfolio with multiple experiences, including a
                   faux IDE & a throwback to 2000&apos;s-era pop-ups.
                 </p>
                 <Link
                   href="https://github.com/oconnorjohnson/danielojohnson.com"
-                  className="text-blue-400 underline"
+                  className="text-blue-400 hover:underline"
                 >
                   GitHub
                 </Link>
@@ -117,18 +126,20 @@ export default function BoringPage() {
             <div className="flex flex-row justify-between w-full border-l-4 border-gray-400">
               <div className="w-1/4 lg:w-2/3 border-t border-gray-400" />
               <div className="flex flex-col border border-gray-400 rounded-bl-lg rounded-br-lg rounded-tr-lg px-6 py-4 mb-8">
-                <h2 className="font-semibold text-xl">Milli-Agent {"(WIP)"}</h2>
-                <h3 className="font-light text-lg">
+                <h2 className="font-semibold text-xl pb-2 text-blue-500">
+                  Milli-Agent {"(WIP)"}
+                </h2>
+                <h3 className="font-light text-lg pb-2">
                   Personal Transcription Agent
                 </h3>
-                <p className="text-gray-400">
+                <p className="text-gray-300 pb-2">
                   TV writing assistant that transcribes, analyzes and tracks
                   5-10 hour brainstorming sessions between myself and my writing
                   partner.
                 </p>
                 <Link
                   href="https://github.com/oconnorjohnson/milli-agent"
-                  className="text-blue-400 underline"
+                  className="text-blue-400 hover:underline"
                 >
                   GitHub
                 </Link>
@@ -137,13 +148,13 @@ export default function BoringPage() {
             <div className="flex flex-row justify-between w-full border-l-4 border-gray-400">
               <div className="w-1/4 lg:w-2/3  border-t border-gray-400" />
               <div className="flex flex-col border border-gray-400 rounded-bl-lg rounded-br-lg rounded-tr-lg px-6 py-4 mb-8">
-                <h2 className="font-semibold text-xl">
+                <h2 className="font-semibold text-xl pb-2 text-blue-500">
                   yolopublicdefendercares.org
                 </h2>
-                <h3 className="font-light text-lg">
+                <h3 className="font-light text-lg pb-2">
                   Asset Acquisition & Distribution Management Platform
                 </h3>
-                <p className="text-gray-400">
+                <p className="text-gray-300 pb-2">
                   A financial and asset acquisition{"/"}distribution management
                   platform for Yolo County&apos;s nonprofit, CARES. I moved our
                   organization&apos;s entire tracking systems off of paper and
@@ -152,7 +163,7 @@ export default function BoringPage() {
                 </p>
                 <Link
                   href="https://yolopublicdefendercares.org"
-                  className="text-blue-400 underline"
+                  className="text-blue-400 hover:underline"
                 >
                   Live Site
                 </Link>
@@ -161,60 +172,69 @@ export default function BoringPage() {
             <div className="flex flex-row justify-between w-full border-l-4 border-gray-400">
               <div className="w-1/4 lg:w-2/3 border-t border-gray-400" />
               <div className="flex flex-col border border-gray-400 rounded-bl-lg rounded-br-lg rounded-tr-lg px-6 py-4 mb-8">
-                <h2 className="font-semibold text-xl">PocketPR</h2>
-                <h3 className="font-light text-lg">
+                <h2 className="font-semibold text-xl pb-2 text-blue-500">
+                  PocketPR
+                </h2>
+                <h3 className="font-light text-lg pb-2">
                   Automated PR for Content Creators
                 </h3>
-                <p className="text-gray-400">
+                <p className="text-gray-300 pb-2">
                   A simple chatbot enhanced with embedding creation to enable
                   context-aware retrieval and interaction based on a user&apos;s
                   YouTube videos and their comments.
                 </p>
                 <Link
                   href="https://pocketpr.app"
-                  className="text-blue-400 underline"
+                  className="text-blue-400 hover:underline"
                 >
                   Live Site
                 </Link>
               </div>
             </div>
+            <div className="py-6 w-full border-b border-gray-400" />
           </div>
-          <div className="py-6 w-full border-b border-gray-400" />
           <div className="py-12 w-full " />
-          <h1
-            id="skills"
-            className="flex flex-col justify-center font-bold text-2xl lg:text-4xl h-screen"
-          >
-            Skills & Interests
-          </h1>
-          <div className="py-2" />
           <div className="flex flex-col justify-center w-3/4 lg:w-1/2">
-            stuffr
+            <h1
+              id="skills"
+              className="flex flex-col justify-center font-bold text-2xl lg:text-4xl"
+            >
+              Skills & Interests
+            </h1>
+            <div className="py-2" />
+            <div className="h-screen flex flex-col justify-center w-3/4 lg:w-1/2">
+              stuffr
+            </div>
+            <div className="py-6 w-full border-b border-gray-400" />
           </div>
-          <div className="py-6 w-full border-b border-gray-400" />
           <div className="py-12 w-full " />
-          <h1
-            id="blogs"
-            className="flex flex-col justify-center font-bold text-2xl lg:text-4xl h-screen"
-          >
-            Blogs
-          </h1>
-          <div className="py-2" />
           <div className="flex flex-col justify-center w-3/4 lg:w-1/2">
-            stuffr
+            <h1
+              id="blogs"
+              className="flex flex-col justify-center font-bold text-2xl lg:text-4xl"
+            >
+              Blogs
+            </h1>
+            <div className="py-2" />
+            <div className="h-screen flex flex-col justify-center w-3/4 lg:w-1/2">
+              stuffr
+            </div>
+            <div className="py-6 w-full border-b border-gray-400" />
           </div>
-          <div className="py-6 w-full border-b border-gray-400" />
           <div className="py-12 w-full " />
-          <h1
-            id="contact"
-            className="flex flex-col justify-center font-bold text-2xl lg:text-4xl h-screen"
-          >
-            Contact Me
-          </h1>
-          <div className="py-6 w-full border-b border-gray-400" />
-          <div className="py-12 w-full " />
-          <a href="#home">Back to Top</a>
-          <div className="py-12" />
+          <div className="flex flex-col justify-center w-3/4 lg:w-1/2">
+            <h1
+              id="contact"
+              className="flex flex-col justify-center font-bold text-2xl lg:text-4xl"
+            >
+              Contact Me
+            </h1>
+            <div className="py-2" />
+            <div className="h-screen flex flex-col justify-center w-3/4 lg:w-1/2">
+              stuffr
+            </div>
+            <div className="py-6 mb-12 w-full border-b border-gray-400" />
+          </div>
         </div>
       </div>
     </>
