ce111479994598a96c21e8055ee7ce1322a5222a
Daniel Johnson <dojohnson@ucdavis.edu>
2024-05-06 21:26:51 -0700

    added cards for simple, code editor, futuristic, and 2000s

--- app/page.tsx
+++ app/page.tsx
@@ -3,26 +3,45 @@ import Link from "next/link";
 export default function RootPage() {
   return (
     <>
-      <div className="flex flex-col py-10 px-6 w-screen h-screen">
+      <div className="flex flex-col py-10 px-6 w-screen h-screen lg:justify-center">
         <h3 className="font-semilight text-md lg:text-xl">
           So you&apos;ve been tasked with learning about me...
         </h3>
+        <div className="py-2" />
         <h1 className="font-bold text-3xl lg:text-6xl">
           Choose Your Experience {":)"}
         </h1>
-        {/* Code Editor Card */}
-        <div className="flex flex-col lg:flex-row lg:gap-x-4">
+
+        <div className="flex flex-col lg:flex-row lg:gap-x-4 pt-8">
+          {/* Plain Text Card*/}
           <Link
-            href="/ide"
-            className="flex flex-grow w-full bg-white text-black px-2 py-1 rounded-md h-[250px] my-4"
+            href="/boring"
+            className="flex flex-grow w-full bg-white text-black px-2 py-1 rounded-md my-2 h-[250px]"
           >
-            Code Editor
+            Simple Portfolio
           </Link>
+          {/* Code Editor Card */}
           <Link
             href="/ide"
-            className="flex flex-grow w-full bg-white text-black px-2 py-1 rounded-md h-[250px] my-4"
+            className="flex flex-grow w-full bg-white text-black px-2 py-1 rounded-md my-2 h-[250px]"
           >
-            Code Editor
+            Code Editor Portfolio
+          </Link>
+        </div>
+        <div className="flex flex-col lg:flex-row lg:gap-x-4">
+          {/* Code Editor Card */}
+          <Link
+            href="/ide"
+            className="flex flex-grow w-full bg-white text-black px-2 py-1 rounded-md my-2 h-[250px]"
+          >
+            Futuristic Portfolio
+          </Link>
+          {/* Code Editor Card */}
+          <Link
+            href="/ide"
+            className="flex flex-grow w-full bg-white text-black px-2 py-1 rounded-md my-2 h-[250px]"
+          >
+            2000&apos;s Portfolio
           </Link>
         </div>
       </div>
