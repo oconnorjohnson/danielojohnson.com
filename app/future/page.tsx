ba3d1874786922af8898a04f440c4f76e6b8c52a
Daniel Johnson <dojohnson@ucdavis.edu>
2024-05-06 23:40:56 -0700

    refactored root page so links are a simple ordered list, installed shadcn hover-card to show more information about linked item on root

--- app/future/page.tsx
+++ app/future/page.tsx
@@ -1,7 +1,7 @@
 export default function ThousandsPage() {
   return (
     <>
-      <div>hello from the 2000&apos;s page</div>
+      <div>hello from the Futuristic page</div>
     </>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function RootPage() {
   return (
     <>
-      <div className="flex flex-col py-10 px-6 w-screen h-screen lg:justify-center">
+      <div className="flex flex-col py-10 px-6 w-screen h-screen">
         <h2 className="font-bold pb-1">
           Daniel O&apos;Connor Johnson | Builder
         </h2>
+
         <h3 className="font-light text-lg lg:text-xl">
           So you&apos;ve been tasked with learning about me...
         </h3>
@@ -14,42 +16,28 @@ export default function RootPage() {
         <h1 className="font-black text-3xl lg:font-bold lg:text-6xl pt-4">
           Choose Your Experience {":)"}
         </h1>
-
-        <div className="flex flex-col lg:flex-row lg:gap-x-8 pt-8">
-          {/* Plain Text Card*/}
-          <Link
-            href="/boring"
-            className="flex flex-grow w-full bg-white text-black px-2 py-1 rounded-md my-4 h-[250px]"
-          >
-            Simple Portfolio
-          </Link>
-          {/* Code Editor Card */}
-          <div className="block flex flex-grow w-full bg-gray-400 text-black px-2 py-1 rounded-md my-4 h-[250px] lg:hidden">
-            Code Editor Portfolio - Available on Laptop/Desktop
-          </div>
-          <Link
-            href="/ide"
-            className="hidden lg:flex flex-grow w-full bg-white text-black px-2 py-1 rounded-md my-4 h-[250px]"
-          >
-            Code Editor Portfolio
-          </Link>
-        </div>
-        <div className="flex flex-col lg:flex-row lg:gap-x-8">
-          {/* Code Editor Card */}
-          <Link
-            href="/ide"
-            className="flex flex-grow w-full bg-white text-black px-2 py-1 rounded-md my-4 h-[250px]"
-          >
-            Futuristic Portfolio
-          </Link>
-          {/* Code Editor Card */}
-          <Link
-            href="/ide"
-            className="flex flex-grow w-full bg-white text-black px-2 py-1 rounded-md my-4 h-[250px]"
-          >
-            2000&apos;s Portfolio
-          </Link>
-        </div>
+        <ol className="list-decimal text-xl px-6 pt-4">
+          <li>
+            <Link className="hover:text-yellow-400 underline" href="/boring">
+              Plain Text
+            </Link>
+          </li>
+          <li>
+            <Link className="hover:text-yellow-400 underline" href="/ide">
+              Code Editor
+            </Link>
+          </li>
+          <li>
+            <Link className="hover:text-yellow-400 underline" href="/future">
+              Futuristic
+            </Link>
+          </li>
+          <li>
+            <Link className="hover:text-yellow-400 underline" href="/2000s">
+              2000&apos;s
+            </Link>
+          </li>
+        </ol>
       </div>
     </>
   );
